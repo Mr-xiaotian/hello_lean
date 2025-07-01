9c735a1e99ceef6565eabead49d71f3df49fadf8
Xiao tian Liu <v-xiaotiliu@microsoft.com>
2025-07-01 15:14:46 +0800

    [Add] zero_add

--- Main.lean
+++ Main.lean
@@ -1,4 +1,13 @@
-import HelloLean
+theorem add_zero (n : Nat) : n + 0 = n := by
+  rfl
 
-def main : IO Unit :=
-  IO.println s!"Hello, {hello}!"
+theorem zero_add (n : Nat) : 0 + n = n := by
+  induction n with
+  | zero =>
+    -- base case: 0 + 0 = 0
+    rfl
+  | succ k ih =>
+    -- inductive step: assume 0 + k = k, show 0 + (k + 1) = k + 1
+    calc
+      0 + Nat.succ k = Nat.succ (0 + k) := rfl
+      _              = Nat.succ k       := by rw [ih]
