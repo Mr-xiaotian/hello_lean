42eac9d213c5b621c300c8b5b6f3ca8f24016eea
Xiaotian <273420001@qq.com>
2025-07-01 16:05:11 +0800

    [Add] theorem add_succ succ_add

--- Main.lean
+++ Main.lean
@@ -1,3 +1,13 @@
+    --                     [A5] n + m = m + n   (加法交换律)
+    --                      ↑
+    --                 ┌────┴────┐
+    --              [A3]         [A4]
+    --     (succ 加法右结合)   (0 + n = n)
+    --           ↑              ↑
+    --          [A1]           [A2]
+    --  (succ n + m = succ (n + m))   (n + 0 = n)
+
+
 theorem add_zero (n : Nat) : n + 0 = n := by
   rfl
 
@@ -18,3 +28,18 @@ example : 1 + 1 = 2 := by
     _     = Nat.succ (1 + 0) := rfl
     _     = Nat.succ 1       := by rw [add_zero]
     _     = 2                := rfl
+
+theorem succ_add (n m : Nat) : Nat.succ n + m = Nat.succ (n + m) := by
+  simp [Nat.succ_add]
+
+theorem add_succ (n m : Nat) : n + Nat.succ m = Nat.succ (n + m) := by
+  induction n with
+  | zero =>
+    calc
+      0 + Nat.succ m = Nat.succ m := by rw [zero_add]
+      _              = Nat.succ (0 + m) := by rw [zero_add]
+  | succ k ih =>
+    calc
+      Nat.succ k + Nat.succ m = Nat.succ (k + Nat.succ m)   := by rw [succ_add]
+      _                       = Nat.succ (Nat.succ (k + m)) := by rw [ih]
+      _                       = Nat.succ (Nat.succ k + m)   := by rw [succ_add]
